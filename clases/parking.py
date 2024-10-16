3b0b1d55891d784f4f4876b04b34faabed5358fb
Hugo <hugo.reyhol@educa.jcyl.es>
2024-10-16 18:02:11 +0200

    Acabado de comentar

--- clases/parking.py
+++ clases/parking.py
@@ -11,6 +11,7 @@ class Parking:
     columnas: int = 0
     nivel: int = 0
 
+    # Constructor de la clase, recibe la lista de niveles
     def __init__(self, niveles_json: str):
         self.niveles_json = niveles_json
 
@@ -71,19 +72,24 @@ class Parking:
     # Devuelve True si se puede mover y si no False
     def mover_coche(self, mov: chr) -> bool:
         try:
+            # Busca el coche que coincide con la letra del movimiento introducido
             coche: Coche = next(filter(lambda c: str(mov).upper()[0] == c.letra, self.coches))
 
+        # Si el coche no existe salta esta excepción
         except StopIteration:
             print(f"Movimiento no válido, no hay un coche {str(mov).upper()}")
             return False
 
+        # Guarda al espacio al que es va a mover el coche
         pos = coche.calcular_espacio(str(mov).isupper())
 
+        # Guarda las coordenadas que ocupan todos los coches menos el que se va a mover
         coords = [coord for c in self.coches for coord in c if c.letra != str(mov).upper()]
-        # Si no está en rango sale de la funcion
+        # Si no está en rango o la posición ya está ocupada por otro coche sale de la funcion
         if (not self.__en_rango(pos)) or (pos in coords):
             return False
 
+        # Manda al coche moverse
         coche.mover(str(mov).isupper())
 
         return True
@@ -96,20 +102,25 @@ class Parking:
 
     # Cambia el formato en el que se devuelve el parking al imprimirlo
     def __str__(self) -> str:
+        # Crea una matriz del tamaño del parking con con # en los bordes
         p = [["#" if (c == 0 or c == self.columnas + 1) or (f == 0 or f == self.filas + 1) else " " for c in
               range(self.columnas + 2)] for f in range(self.filas + 2)]
+        # Vacía la casilla de salida
         p[self.salida[0]][self.salida[1]] = " "
 
+        # Por cada coche en la lista obtiene sus coordenadas y su representación
         for coche in self.coches:
             coord = [coord for coord in coche]
             letras = list(f"{coche}")
 
+            # En cada coordenada dibuja en el parking su parte del coche
             for _ in range(len(coord)):
                 c = coord.pop(0)
                 p[c[0]][c[1]] = letras.pop(0)
 
+        # Convierte la matriz en un string
         cadena = ""
         for l in p:
-            cadena += " ".join(l)
-            cadena += "\n"
+            cadena += " ".join(l) + "\n"
+
         return cadena
--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import json
 import time
-
 from clases.parking import Parking
 
 
